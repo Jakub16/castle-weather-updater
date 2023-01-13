113842fa7df0581b767e84fcc48125f7568273da
kubaz <jakub.zawadzki16@gmail.com>
2023-01-13 13:11:36 +0100

    castle-weather-updater module add

---
+++ src/main/java/com/castle/weatherupdater/CastleWeatherUpdaterApplication.java
@@ -0,0 +1,13 @@
+package com.castle.weatherupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleWeatherUpdaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CastleWeatherUpdaterApplication.class, args);
+    }
+
+}
